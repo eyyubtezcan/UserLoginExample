2ea648b1bbf973a97432e4ec0fcd92895728e8e9
TEZCAN <eyyub.tezcan@gmail.com>
2018-10-15 05:51:21 +0300

    User Login Sesion
    User Login Sesion

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+
+namespace UserLoginExample.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        string[] name = { "admin", "teacher", "student1", "student2" };
+        string[] pass = { "admin", "123456", "0000", "test" };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string userName,string userPass)
+        {
+            if((userName !="") && (userPass!="" ))
+            { 
+                if(Session["UserName"] == null)
+                {
+                    for(int i=0;i<name.Length;i++)
+                    {
+                        if((name[i]==userName)&&(pass[i]==userPass))
+                        {
+                            Session["UserName"] = userName;
+                            break;
+                        }
+                    }
+                }
+            }
+           
+            return View();
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserLoginExample
+{
+    public class Users
+    {
+
+        public string[] name = { "admin", "teacher", "student1", "student2" };
+        public string[] pass = { "admin", "123456", "0000", "test" };
+
+
+    }
+}
